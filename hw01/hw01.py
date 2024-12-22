df74f6e40f847caabee79794c66d57a4c0e07709
IZSSERAFIM <237422698@qq.com>
2024-12-22 23:57:22 +0800

    feat: finish hw1 and lab1

--- hw01/hw01.py
+++ hw01/hw01.py
@@ -13,9 +13,9 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = sub
     else:
-        f = _____
+        f = add
     return f(a, b)
 
 def a_plus_abs_b_syntax_check():
@@ -42,7 +42,7 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    return i*i + j*j + k*k - max(i, j, k)*max(i, j, k)
 
 def two_of_three_syntax_check():
     """Check that your two_of_three code consists of nothing but a return statement.
@@ -66,6 +66,12 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    if(n % 2 == 0):
+        return n // 2
+    i = (n - 1) // 2
+    while(n % i != 0):
+        i -= 1
+    return i
 
 
 def hailstone(n):
@@ -88,4 +94,14 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    step = 1
+    while(n != 1):
+        print(n)
+        if(n % 2 == 0):
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        step += 1
+    print(n)
+    return step
 
