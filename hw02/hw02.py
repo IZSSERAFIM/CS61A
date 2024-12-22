d56c15125fdd9ade37ccabb8bded04ab8319b578
IZSSERAFIM <237422698@qq.com>
2024-12-23 03:07:28 +0800

    feat: finish hw2

--- hw02/hw02.py
+++ hw02/hw02.py
@@ -32,7 +32,11 @@ def product(n, term):
     162
     """
     "*** YOUR CODE HERE ***"
-
+    ans = 1
+    while(n != 0):
+        ans *= term(n)
+        n -= 1
+    return ans
 
 def accumulate(fuse, start, n, term):
     """Return the result of fusing together the first n terms in a sequence 
@@ -54,6 +58,11 @@ def accumulate(fuse, start, n, term):
     19
     """
     "*** YOUR CODE HERE ***"
+    ans = start
+    while(n != 0):
+        ans = fuse(ans, term(n))
+        n -= 1
+    return ans
 
 
 def summation_using_accumulate(n, term):
@@ -68,7 +77,7 @@ def summation_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(summation_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    return ____
+    return accumulate(add, 0, n, term)
 
 
 def product_using_accumulate(n, term):
@@ -83,7 +92,7 @@ def product_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(product_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    return ____
+    return accumulate(mul, 1, n, term)
 
 
 def make_repeater(f, n):
@@ -100,4 +109,10 @@ def make_repeater(f, n):
     390625
     """
     "*** YOUR CODE HERE ***"
+    def g(x, n=n):
+        while(n != 0):
+            x = f(x)
+            n -= 1
+        return x
+    return g
 
